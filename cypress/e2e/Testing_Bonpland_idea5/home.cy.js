4d157560629e155da12bc9739257b6087f9d35b1
Maxi.Barbo <maxi10lb@gmail.com>
2022-10-23 19:10:47 -0300

    testing bonpland sprint1

---
+++ cypress/e2e/Testing_Bonpland_idea5/home.cy.js
@@ -0,0 +1,15 @@
+describe('Testing en Home', () => {
+
+    beforeEach(() => {
+        // cy.visit('https://maxibarbo.github.io/Cotizar_divisas/index.html')
+
+      })
+
+    it('Visibilidad Items en Home', ()=>{
+
+      
+
+    })
+
+})
+  
