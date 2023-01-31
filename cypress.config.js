1c659ef9bb7d4a5111f36b62301629cc65a4da11
Maxi.Barbo <maxi10lb@gmail.com>
2023-01-31 15:59:17 -0300

    test 9

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,26 @@
 const { defineConfig } = require("cypress");
+const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  // reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl:'https://dev.tahouse.casa/',
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
-      
+    require('cypress-mochawesome-reporter/plugin')(on);
+    cypressSplit(on, config)
+      return config
     },
+    reporter:'mochawesome',
     reporterOptions:{
       charts:true,
       overwrite:false,
       html:true,
       json:true,
-      reporterDir:"cypress/mochawesome-report"
-
+      // reporterDir:"cypress/mochawesome-report"
+      reportDir: 'cypress/results'
     },
       // viewportHeight:670,
       // viewportWidth:380,
       chromeWebSecurity:false
   },
-
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -23,3 +23,30 @@ require('cypress-xpath')
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+import addContext from 'mochawesome/addContext'
+
+const titleToFileName = (title) =>
+  title.replace(/[:\/]/g, '')
+
+Cypress.on('test:after:run', (test, runnable) => {
+  if (test.state === 'failed') {
+    let parent = runnable.parent
+    let filename = ''
+    while (parent && parent.title) {
+      filename = `${titleToFileName(
+        parent.title,
+      )} -- ${filename}`
+      parent = parent.parent
+    }
+    filename += `${titleToFileName(
+      test.title,
+    )} (failed).png`
+    addContext(
+      { test },
+      `../screenshots/${Cypress.spec.name}/${filename}`,
+    )
+  }
+  // always add the video
+  addContext({ test }, `../videos/${Cypress.spec.name}.mp4`)
+})
